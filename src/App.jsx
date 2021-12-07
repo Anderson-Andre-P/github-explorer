5b17e671756e2de6559f1a6c87661e2214b7b4b4
Anderson-Andre-P <andreandersoncaue.e@gmail.com>
2021-12-07 14:54:15 -0300

    feat: component state

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+import { Counter } from './components/Counter'
+import { RepositoryItem } from './components/RepositoryItem'
 import { RepositoryList } from './components/RepositoryList'
 import './styles/global.scss'
 
 export function App() {
-    return <RepositoryList />
+    return (
+        <>
+            <RepositoryList />
+            <Counter />
+        </>
+    )
 }
