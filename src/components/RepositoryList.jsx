e6e4e3ad7722f77eefc3a3a5fad9bfc3411f42e3
Anderson-Andre-P <andreandersoncaue.e@gmail.com>
2021-12-07 15:39:31 -0300

    feat: utilizando o useEffect

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,13 +1,24 @@
 import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss'
+import { useState, useEffect } from "react";
 
 const repository = {
     name: 'unform',
     description: "Forms in React" ,
     link: "https://github.com/unform/unform"
 }
+// https://api.github.com/users/Anderson-Andre-P
 
 export function RepositoryList() {
+
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.github.com/users/Anderson-Andre-P/repos')
+            .then(response => response.json())
+            .then(data => setRepositories(data))
+    }, [])
+
     return (
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
