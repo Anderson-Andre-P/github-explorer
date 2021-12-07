c2bcaec570f50adcd623176fb3bd48610b58cd4b
Anderson-Andre-P <andreandersoncaue.e@gmail.com>
2021-12-07 14:31:06 -0300

    feat: first component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
+import { RepositoryList } from './components/RepositoryList'
 import './styles/global.scss'
 
 export function App() {
-    // throw new Error('Erro proposital')
-    
-    return <h1>Hello World</h1>
+    return <RepositoryList />
 }
